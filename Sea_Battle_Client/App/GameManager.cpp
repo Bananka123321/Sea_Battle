744c1e6f31a10aa9951bb1f9850112058c17d6a6
Bananka123321 <vanushabrend@gmail.com>
2026-07-18 22:47:56 +0700

    Start connect server and client

--- Sea_Battle_Client/App/GameManager.cpp
+++
@@ -1,8 +0,0 @@
-#include "GameManager.h"
-
-GameManager::GameManager(Handler* handler, MainWindow* window) : handler_(handler), window_(window) {
-    connect(handler_, &Handler::S_LobbyCreated, this, [this](const std::string& code){
-
-    });
-}
-
--- Sea_Battle_Client/App/Handler.h
+++ Sea_Battle_Client/App/Handler.h
@@ -28,6 +28,9 @@ private:
     void onPlayerJoined(const std::string& username);
     void onPlayerLeft();
     void onPlayerReadyResponse(bool success);
+    void onGameStarted(bool yourTurn);
+    void onShotResult(int row, int column, int result, bool yourTurn);
+    void onGameOver(const std::string& winner);
 
 signals:
     void S_Message(const int sender, const int receiver, const std::string& text);
@@ -39,5 +42,8 @@ signals:
     void S_PlayerJoined(const std::string& username);
     void S_PlayerLeft();
     void S_EnemyReady(bool success);
+    void S_GameStarted(bool yourTurn);
+    void S_ShotResult(int row, int column, int result, bool yourTurn);
+    void S_onGameOver(const std::string& winner);
 
 };
